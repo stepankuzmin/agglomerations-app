c2722f6b6641aac631df61c9764a41d3e5837d4f
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2018-10-28 19:54:10 +0300

    chore: cleanup

--- App.js
+++ App.js
@@ -17,16 +17,19 @@ const App = () => {
       styleURL='mapbox://styles/mufmap/cj3mqedw800032sonq89ukwh7'
       {...viewport}
     >
-      {/* <MapboxGL.VectorSource
-        id='gamma.urban_expansion'
-        url={sources.urban_expansion}
+      <MapboxGL.VectorSource
+        id='gamma.density_seasons'
+        url='https://agglomerations.org/tiles/gamma.density_seasons.json'
       >
-        <MapboxGL.FillLayer
-          id='gamma.urban_expansion'
-          sourceLayerID='gamma.urban_expansion'
-          style={layerStyles.boxFill}
+        <MapboxGL.FillExtrusionLayer
+          id='gamma.density_seasons'
+          sourceID='gamma.density_seasons'
+          sourceLayerID='gamma.density_seasons'
+          belowLayerID='road_major_base'
+          style={mapStyles.fillExtrusion}
+          filter={['all', ['>', 'density', 0], ['in', 'season', 0, 1]]}
         />
-      </MapboxGL.VectorSource> */}
+      </MapboxGL.VectorSource>
     </MapboxGL.MapView>
   );
 };
@@ -37,11 +40,31 @@ const styles = StyleSheet.create({
   }
 });
 
-// const layerStyles = MapboxGL.StyleSheet.create({
-//   boxFill: {
-//     fillColor: 'green',
-//     fillAntialias: true
-//   }
-// });
+const mapStyles = MapboxGL.StyleSheet.create({
+  fillExtrusion: {
+    visibility: 'visible',
+    fillExtrusionOpacity: 0.55,
+    fillExtrusionColor: MapboxGL.StyleSheet.source(
+      [
+        [0, '#CCF7FF'],
+        [10, '#7FFF9E'],
+        [500, '#00F0FF'],
+        [1500, '#00B2FF'],
+        [3000, '#0D8CFF'],
+        [5000, '#194CFF'],
+        [10000, '#5525F2'],
+        [20000, '#8500CC'],
+        [35000, '#B800D9']
+      ],
+      'density',
+      MapboxGL.InterpolationMode.Interval
+    ),
+    fillExtrusionHeight: MapboxGL.StyleSheet.source(
+      [[0, 0], [10000, 10000], [40000, 40000], [100000, 60000]],
+      'density',
+      MapboxGL.InterpolationMode.Exponential
+    )
+  }
+});
 
 export default App;
--- config.js
+++ config.js
@@ -3,13 +3,7 @@ import Config from 'react-native-config';
 export const MAPBOX_ACCESS_TOKEN = Config.MAPBOX_ACCESS_TOKEN;
 
 export const viewport = {
-  centerCoordinate: [37.617635, 55.755814],
-  zoomLevel: 7,
+  centerCoordinate: [-74, 40.8],
+  zoomLevel: 8,
   pitch: 40
-};
-
-export const sources = {
-  'gamma.urban_expansion': {
-    url: 'https://agglomerations.org/tiles/gamma.urban_expansion.json'
-  }
 };
